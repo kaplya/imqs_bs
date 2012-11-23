130dd2b95bdf69b1a8ad78f8cf4ca62fcad12b29
Vadim Vdovin <vadim.a.vdovin@gmail.com>
2012-11-24 00:12:11 +0400

    journals list done

--- app/scripts/controllers/journals.js
+++ app/scripts/controllers/journals.js
@@ -4,5 +4,10 @@ imqsBsApp.controller('JournalsCtrl', function($scope) {
   $scope.templates = {};
   $scope.templates.sb = "views/journals_sb.html";
   $scope.templates.body = "views/journals.html";
-  console.log("JournalsCtrl");
+  
+  $scope.journals = [
+  	{ number: "0001", date: "01.11.2012", j_type: "Supply", posted: "true" },
+  	{ number: "0002", date: "02.11.2012", j_type: "Movement", posted: "false" },
+  ]
+
 });
