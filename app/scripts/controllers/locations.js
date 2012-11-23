f5ce94b80e1e706e3c732eb1a4e35797cb756e3f
Vadim Vdovin <vadim.a.vdovin@gmail.com>
2012-11-23 22:32:01 +0400

    (feat) location form done

--- app/scripts/controllers/locations.js
+++ app/scripts/controllers/locations.js
@@ -4,4 +4,8 @@ imqsBsApp.controller('LocationsCtrl', function($scope) {
   $scope.templates = {};
   $scope.templates.body = "views/locations.html";
   // $scope.templates.sb = "'views/locations_sb.html'";
+  $scope.locations = [
+  	{ name: "West Store", city: "Tokyo"},
+  	{ name: "Europe Store", city: "Paris"}
+  ];
 });
