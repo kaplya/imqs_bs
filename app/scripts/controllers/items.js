fa8cb0706aa35cf65014d0bb12329fa7a2b42f84
Vadim Vdovin <vadim.a.vdovin@gmail.com>
2012-12-25 16:01:49 +0400

    feat(items): spin while loading items

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -4,80 +4,83 @@ imqsBsApp
   .factory('Item', ['$resource', function ($resource) {
 	return $resource('/items/:id/:action');  	
   }])
-  .controller('ItemsCtrl', ["$scope", 'Item', function($scope, Item) {
-  
-  $scope.templates = {
-  	body: "views/items.html"
-  };
+  .controller('ItemsCtrl', ["$scope", 'Item', '$timeout', function($scope, Item, $timeout) {
 
-  $scope.modalEdit = { shown: false };
-  $scope.modalDel = { shown: false };
 
-  Item.query(function(data) {
-  	$scope.items = data;
-  });
+    $scope.templates = {
+      body: "views/items.html"
+    };
 
-  $scope.edit = function () {
-    $scope.modalEdit.listData = this.i;
-    Item.get( { id: this.i.id }, function (data) {
-      $scope.modalEdit.d = data;
+    $scope.modalEdit = { shown: false };
+    $scope.modalDel = { shown: false };
+    $scope.spin = true;
+
+    Item.query(function(data) {
+      $scope.items = data;
+      $scope.spin = false;
     });
-    this.modalEdit.shown = true;
-  };
-  
-  $scope.new = function () {
-    this.modalEdit.d = {};
-    this.modalEdit.shown = true;
-  };
 
-  $scope.createOrUpdate = function () {
-    $scope.modalEdit.d.error = undefined;
+    $scope.edit = function () {
+      $scope.modalEdit.listData = this.i;
+      Item.get( { id: this.i.id }, function (data) {
+        $scope.modalEdit.d = data;
+      });
+      this.modalEdit.shown = true;
+    };
     
-    var update = function () { 
-      Item.update({ id: $scope.modalEdit.d.id }, $scope.modalEdit.d, function (data) {
-        $scope.modalEdit.shown = false;
-        angular.copy(data, $scope.modalEdit.listData);
-      }, function (r) {
-        $scope.modalEdit.d.error = r.data;
-      });
+    $scope.new = function () {
+      this.modalEdit.d = {};
+      this.modalEdit.shown = true;
     };
 
-    var create = function () { 
-      Item.create($scope.modalEdit.d, function (data) { 
-        $scope.modalEdit.shown = false;
-        $scope.items.unshift(data);
-      }, function (r) {
-        $scope.modalEdit.d.error = r.data;
-      });
-    };
-
-    if ($scope.modalEdit.d.id) update(); 
-    else create();
-
-  };
-
-  $scope.delete = function () {
-    $scope.modalDel.error = undefined;
-    this.modalDel.shown = true;
-    this.modalDel.listData = this.i;
-  };
-
-  $scope.destroy = function () {
-    $scope.modalDel.error = undefined;
-    var index,
-      id = $scope.modalDel.listData.id;
-    Item.destroy({ id: id }, function () {
-        angular.forEach($scope.items, function(v,i) {
-          if (v.id == id) {
-            index = i;
-            return false;
-          }
+    $scope.createOrUpdate = function () {
+      $scope.modalEdit.d.error = undefined;
+      
+      var update = function () { 
+        Item.update({ id: $scope.modalEdit.d.id }, $scope.modalEdit.d, function (data) {
+          $scope.modalEdit.shown = false;
+          angular.copy(data, $scope.modalEdit.listData);
+        }, function (r) {
+          $scope.modalEdit.d.error = r.data;
         });
-        $scope.items.splice(index,1);
-        $scope.modalDel.shown = false;
-      }, function (r) {
-        $scope.modalDel.error = r.data;
-    });
-  };
+      };
 
-}]);
+      var create = function () { 
+        Item.create($scope.modalEdit.d, function (data) { 
+          $scope.modalEdit.shown = false;
+          $scope.items.unshift(data);
+        }, function (r) {
+          $scope.modalEdit.d.error = r.data;
+        });
+      };
+
+      if ($scope.modalEdit.d.id) update(); 
+      else create();
+
+    };
+
+    $scope.delete = function () {
+      $scope.modalDel.error = undefined;
+      this.modalDel.shown = true;
+      this.modalDel.listData = this.i;
+    };
+
+    $scope.destroy = function () {
+      $scope.modalDel.error = undefined;
+      var index,
+        id = $scope.modalDel.listData.id;
+      Item.destroy({ id: id }, function () {
+          angular.forEach($scope.items, function(v,i) {
+            if (v.id == id) {
+              index = i;
+              return false;
+            }
+          });
+          $scope.items.splice(index,1);
+          $scope.modalDel.shown = false;
+        }, function (r) {
+          $scope.modalDel.error = r.data;
+      });
+    };
+
+  }]);
--- app/scripts/directives/spin.js
+++ app/scripts/directives/spin.js
@@ -3,11 +3,27 @@ angular.module('ui')
 		return {
 			restrict: 'A',
 			link: function (scope, iElm, iAttr, ctrl) {
-				var spinner = new Spinner();
+				var opts = {
+				  lines: 13, // The number of lines to draw
+				  length: 7, // The length of each line
+				  width: 4, // The line thickness
+				  radius: 10, // The radius of the inner circle
+				  corners: 1, // Corner roundness (0..1)
+				  rotate: 0, // The rotation offset
+				  color: '#000', // #rgb or #rrggbb
+				  speed: 1, // Rounds per second
+				  trail: 60, // Afterglow percentage
+				  shadow: false, // Whether to render a shadow
+				  hwaccel: false, // Whether to use hardware acceleration
+				  className: 'spinner', // The CSS class to assign to the spinner
+				  zIndex: 1000, // The z-index (defaults to 2000000000)
+				  top: 'auto', // Top position relative to parent in px
+				  left: 'auto' // Left position relative to parent in px
+				};
+				var spinner = new Spinner(opts);
 				scope.$watch(iAttr.spin, function (newValue) {
 					if (newValue == true || newValue == 'true') {
-						spinner.spin();
-						iElm.append(spinner.el);
+						spinner.spin(iElm[0]);
 					}
 					else {
 						spinner.stop();
